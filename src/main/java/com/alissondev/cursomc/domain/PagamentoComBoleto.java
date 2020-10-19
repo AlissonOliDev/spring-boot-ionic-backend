77ac9ffc62bbe1fbe17466ceb5d630834d5fff17
Alisson Oliveira <alissonoliveira.dev@gmail.com>
2020-10-19 18:20:57 -0300

    EndPoint /pedidos/{id} disponibilizado

--- src/main/java/com/alissondev/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/alissondev/cursomc/domain/PagamentoComBoleto.java
@@ -5,12 +5,16 @@ import java.util.Date;
 import javax.persistence.Entity;
 
 import com.alissondev.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date dataVencimeno;
+	
+	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {		
