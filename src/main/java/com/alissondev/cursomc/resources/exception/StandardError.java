d12a02a59664c1c7e08bbd23dcc3b33e3619f4c0
Alisson Oliveira <alissonoliveira.dev@gmail.com>
2020-10-21 13:31:33 -0300

    Personalizando o retorno da validacao

--- src/main/java/com/alissondev/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/alissondev/cursomc/resources/exception/StandardError.java
@@ -2,9 +2,9 @@ package com.alissondev.cursomc.resources.exception;
 
 import java.io.Serializable;
 
-public class StandardError implements Serializable {
-	
+public class StandardError implements Serializable {	
 	private static final long serialVersionUID = 1L;
+	
 	private Integer status;
 	private String msg;
 	private Long timeStamp;
