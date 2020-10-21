bc65b22b4e32e3fd1da825d4353e69a8b3badba2
Alisson Oliveira <alissonoliveira.dev@gmail.com>
2020-10-20 22:33:14 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/alissondev/cursomc/services/CategoriaService.java
+++ src/main/java/com/alissondev/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.alissondev.cursomc.domain.Categoria;
+import com.alissondev.cursomc.dto.CategoriaDTO;
 import com.alissondev.cursomc.repositories.CategoriaRepository;
 import com.alissondev.cursomc.services.exceptions.DataIntegrityException;
 import com.alissondev.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
