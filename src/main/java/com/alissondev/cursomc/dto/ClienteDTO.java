8bb9a4c9ffc4c5967f081bfefaaf6e6b27c9abd3
Alisson Oliveira <alissonoliveira.dev@gmail.com>
2020-10-22 18:27:23 -0300

    Validacao customizada: email nao repetido na atualizacao de cliente

--- src/main/java/com/alissondev/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/alissondev/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.alissondev.cursomc.domain.Cliente;
+import com.alissondev.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {	
 	private static final long serialVersionUID = 1L;
 	
