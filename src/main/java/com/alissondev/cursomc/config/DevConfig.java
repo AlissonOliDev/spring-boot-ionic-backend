e41559c6f7161b89cdb7136cd0160f2fff1e8461
Alisson Oliveira <alissonoliveira.dev@gmail.com>
2020-10-26 17:49:40 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/alissondev/cursomc/config/DevConfig.java
+++ src/main/java/com/alissondev/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.alissondev.cursomc.services.DBService;
+import com.alissondev.cursomc.services.EmailService;
+import com.alissondev.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -31,4 +33,8 @@ public class DevConfig {
 		return true;
 	}
 	
+	@Bean
+	public EmailService meailService() {
+		return new SmtpEmailService();
+	}
 }
