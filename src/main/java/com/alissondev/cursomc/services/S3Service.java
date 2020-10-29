6398f6fca377a99a153938e04c4f72de3c2958bd
Alisson Oliveira <alissonoliveira.dev@gmail.com>
2020-10-29 17:55:14 -0300

    Tratando excecoes adequadamente

--- src/main/java/com/alissondev/cursomc/services/S3Service.java
+++ src/main/java/com/alissondev/cursomc/services/S3Service.java
@@ -12,8 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
+import com.alissondev.cursomc.services.exceptions.FileException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
@@ -35,7 +34,7 @@ public class S3Service {
 			String contentType = multpartFile.getContentType();
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO" + e.getMessage());
+			throw new FileException("Erro de IO" + e.getMessage());
 		}
 
 	}
